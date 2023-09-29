70c12f2118676ed0c5de20b8127027656d3b871d
taiduongg <git.taidv1211@gmail.com>
2023-09-29 14:16:41 +0700

    project

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+
+import controller.SolvingEquation;
+/**
+ *
+ * @author lamhnt
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        SolvingEquation se = new SolvingEquation();
+        se.run();
+                
+    }
+    
+
+}
